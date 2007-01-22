bee4ad4ba0b9c2fccbc2be1a5a9092d5030914b2
erdgeist <>
2007-01-22 02:41:09 +0000

    Now allow * in torrents

--- scan_urlencoded_query.c
+++ scan_urlencoded_query.c
@@ -11,7 +11,7 @@
          mark          = "-" | "_" | "." | "!" | "~" | "*" | "'" | "(" | ")"
    we add '%' to the matrix to not stop at encoded chars.
 */
-static const unsigned char reserved_matrix[] = { 0xA2, 0x63, 0xFF, 0x03, 0xFE, 0xFF, 0xFF, 0x87, 0xFE, 0xFF, 0xFF, 0x47};
+static const unsigned char reserved_matrix[] = { 0xA2, 0x67, 0xFF, 0x03, 0xFE, 0xFF, 0xFF, 0x87, 0xFE, 0xFF, 0xFF, 0x47};
 static int is_unreserved( unsigned char c ) {
   if( ( c <= 32 ) || ( c >= 127 ) ) return 0; return 1&(reserved_matrix[(c-32)>>3]>>(c&7));
 }
