6b2307015fb25caf0322b534135f64ba3d7c8136
erdgeist <>
2007-10-21 04:37:37 +0000

    now accept more classes of announce fuckups

--- opentracker.c
+++ opentracker.c
@@ -532,8 +532,7 @@ ANNOUNCE_WORKAROUND:
     ot_overall_tcp_successfulannounces++;
     break;
   case 9:
-    if( !byte_diff( data, 9, "announce " ) ) goto ANNOUNCE_WORKAROUND;
-    if( !byte_diff( data, 9, "announce/" ) ) goto ANNOUNCE_WORKAROUND;
+    if( !byte_diff( data, 8, "announce" ) ) goto ANNOUNCE_WORKAROUND;
     HTTPERROR_404;
   case 10:
     if( byte_diff( data, 10, "scrape.php" ) ) HTTPERROR_404;
